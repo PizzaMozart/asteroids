2eda596f14a4cb3f49e8a79b33f2d1781f555112
PizzaMozart <pizza.mozart.game@gmail.com>
2025-02-10 14:47:36 +0100

    After Player Chapter

--- main.py
+++ main.py
@@ -3,17 +3,36 @@
 # throughout this file
 import pygame
 from constants import *
+from circleshape import*
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(000)
+        screen.fill("black")
+
+        updatable.update(dt)
+
+        for sprite in drawable:
+            sprite.draw(screen)
+            
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
